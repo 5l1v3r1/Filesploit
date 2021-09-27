1e5b7a9db192ae30877bdc8817348cf87344023f
Xale Security <81574241+xaletr@users.noreply.github.com>
2021-09-27 15:03:02 +0300

    Create install.py

---
+++ install.py
@@ -0,0 +1,30 @@
+
+
+import os
+
+import time
+
+
+
+os.system("clear")
+
+print(" ")
+
+s = input("Install Filesploit: (Y/N) ")
+
+
+
+if s == "Y":
+
+ os.system("clear")
+
+ time.sleep(1)
+
+ os.system("sudo apt-get install figlet")
+
+
+
+if s == "N":
+
+ print("Okey.")
+
